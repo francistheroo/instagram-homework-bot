89ad582237e646b28eb86fcab3df5075208a80ea
francistheroo <francistheroo@gmail.com>
2026-08-04 12:58:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 genai.configure(api_key=os.environ["GEMINI_API_KEY"])
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 @app.route("/")
 def home():
